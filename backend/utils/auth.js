3b30e7ce144a2d4e127b8c200c1b7849b57346bc
Austin <hnitsua13@gmail.com>
2024-12-05 17:03:51 -0800

    route files opened, will fill them soon

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,7 +1,15 @@
 // backend/utils/auth.js
 const jwt = require("jsonwebtoken");
 const { jwtConfig } = require("../config");
-const { User } = require("../db/models");
+const {
+  User,
+  Group,
+  Venue,
+  GroupImage,
+  Membership,
+  Event,
+  EventImage,
+} = require("../db/models");
 
 const { secret, expiresIn } = jwtConfig;
 
@@ -71,4 +79,282 @@ const requireAuth = function (req, _res, next) {
   return next(err);
 };
 
-module.exports = { setTokenCookie, restoreUser, requireAuth };
+const strictGroupAuth = async function (req, res, next) {
+  const { user } = req;
+  let group, status;
+
+  const groupId = parseInt(req.params.groupId);
+  if (groupId) {
+    group = await Group.findByPk(groupId);
+    status = await Membership.findOne({
+      where: {
+        userId: user.id,
+        groupId: group.id,
+      },
+    });
+  }
+
+  if (group) {
+    if (user.id == group.organizerId) {
+      return next();
+    } else {
+      const err = new Error("Unauthorized");
+      err.title = "Require proper authorization";
+      err.status = 403;
+      err.errors = { message: "Require proper authorization" };
+      return next(err);
+    }
+  }
+};
+
+const groupAuth = async function (req, res, next) {
+  const { user } = req;
+  let group, status;
+
+  const groupId = parseInt(req.params.groupId);
+  if (groupId) {
+    group = await Group.findByPk(groupId);
+    status = await Membership.findOne({
+      where: {
+        userId: user.id,
+        groupId: group.id,
+        status: "co-host",
+      },
+    });
+  }
+
+  if (group) {
+    if (user.id == group.organizerId || status) {
+      return next();
+    } else {
+      const err = new Error("Unauthorized");
+      err.title = "Require proper authorization";
+      err.status = 403;
+      err.errors = { message: "Require proper authorization" };
+      return next(err);
+    }
+  }
+};
+
+const venueAuth = async function (req, res, next) {
+  const { user } = req;
+  let venue, venueGroup, status;
+
+  const venueId = parseInt(req.params.venueId);
+  if (venueId) {
+    venue = await Venue.findByPk(venueId);
+    venueGroup = await Group.findByPk(venue.groupId);
+  }
+
+  status = await Membership.scope("specific").findOne({
+    where: {
+      userId: user.id,
+      groupId: venue.groupId,
+    },
+  });
+
+  if (venueGroup) {
+    if (
+      venueGroup.organizerId == user.id ||
+      (status && status.status == "co-host")
+    ) {
+      return next();
+    }
+  } else {
+    const err = new Error("Unauthorized");
+    err.title = "Require proper authorization";
+    err.status = 403;
+    err.errors = { message: "Require proper authorization" };
+    return next(err);
+  }
+};
+
+const eventAuth = async function (req, res, next) {
+  const { user } = req;
+  let event, attendance;
+
+  const eventId = parseInt(req.params.eventId);
+  if (eventId) {
+    event = await Event.findByPk(eventId);
+  }
+
+  if (event) {
+    attendance = await event.getUsers({
+      where: {
+        id: user.id,
+      },
+    });
+    if (req.method === "POST") {
+      if (attendance.length) {
+        return next();
+      } else {
+        const err = new Error("Unauthorized");
+        err.title = "Require proper authorization";
+        err.status = 403;
+        err.errors = { message: "Require proper authorization" };
+        return next(err);
+      }
+    } else if (req.method === "PUT" || req.method === "DELETE") {
+      let group = await event.getGroup();
+
+      let memberships = await Membership.findOne({
+        where: {
+          userId: user.id,
+          groupId: group.id,
+          status: "co-host",
+        },
+      });
+
+      if (memberships || group.organizerId === user.id) {
+        return next();
+      } else {
+        const err = new Error("Unauthorized");
+        err.title = "Require proper authorization";
+        err.status = 403;
+        err.errors = { message: "Require proper authorization" };
+        return next(err);
+      }
+    }
+  }
+};
+
+const groupImageAuth = async function (req, res, next) {
+  const { user } = req;
+  const imageId = req.params.imageId;
+  const image = await GroupImage.scope("specific").findByPk(imageId);
+
+  if (!image) {
+    res.status(404);
+    return res.json({
+      message: "Group image not found",
+    });
+  }
+
+  let groupId = image.groupId;
+  let group = await Group.findByPk(groupId);
+  let cohost = await Membership.findOne({
+    where: {
+      userId: user.id,
+      groupId: groupId,
+      status: "co-host",
+    },
+  });
+
+  if (cohost || group.organizerId == user.id) {
+    return next();
+  } else {
+    const err = new Error("Unauthorized");
+    err.title = "Require proper authorization";
+    err.status = 403;
+    err.errors = { message: "Require proper authorization" };
+    return next(err);
+  }
+};
+
+const eventImageAuth = async function (req, res, next) {
+  const { user } = req;
+  const imageId = req.params.imageId;
+  const image = await EventImage.findByPk(imageId);
+
+  if (!image) {
+    res.status(404);
+    return res.json({
+      message: "Event image not found",
+    });
+  }
+
+  let eventId = image.eventId;
+  let event = await Event.findByPk(eventId);
+  let groupId = event.groupId;
+  let group = await Group.findByPk(groupId);
+  let cohost = await Membership.findOne({
+    where: {
+      userId: user.id,
+      groupId: group.id,
+      status: "co-host",
+    },
+  });
+
+  if (cohost || group.organizerId == user.id) {
+    return next();
+  } else {
+    const err = new Error("Unauthorized");
+    err.title = "Require proper authorization";
+    err.status = 403;
+    err.errors = { message: "Require proper authorization" };
+    return next(err);
+  }
+};
+
+const idCheck = async function (req, res, next) {
+  try {
+    const id = parseInt(req.params.groupId);
+
+    const group = await Group.findByPk(id);
+    if (group) {
+      return next();
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    res.status(404);
+    return res.json({
+      message: "Group not found",
+    });
+  }
+};
+
+const venueIDCheck = async function (req, res, next) {
+  try {
+    let id = req.params.venueId || req.body.venueId;
+    id = parseInt(id);
+
+    const venue = await Venue.findByPk(id);
+    if (venue) {
+      return next();
+    } else {
+      throw new Error();
+    }
+  } catch {
+    err;
+  }
+  {
+    res.status(404);
+    return res.json({
+      message: "Venue not found",
+    });
+  }
+};
+
+const eventIDCheck = async function (req, res, next) {
+  try {
+    const id = parseInt(req.params.eventId);
+
+    const event = await Event.findByPk(id);
+    if (event) {
+      return next();
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    res.status(404);
+    return res.json({
+      message: "Event not found",
+    });
+  }
+};
+
+module.exports = {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+  strictGroupAuth,
+  groupAuth,
+  venueAuth,
+  eventAuth,
+  groupImageAuth,
+  eventImageAuth,
+  idCheck,
+  venueIDCheck,
+  eventIDCheck,
+};
